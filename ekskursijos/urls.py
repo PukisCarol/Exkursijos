3c4e9ae29305fef6fedb1071140c86419706a230
ValiusR <valius.ramanauskas@gmail.com>
2026-05-11 20:52:10 +0300

    Implemented deletion basics

--- ekskursijos/urls.py
+++ ekskursijos/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views.user.excursion import (
     addExcursion,
     deleteExcursion,
+    deletePlaylistItem,
     getExcursionList,
     openExcursion,
     mainPage,
     openJoinExcursionPage,
     pupilsListPage,
     openExcursionPlaylist,
+    openPlaylistItemAddPage,
 )
 from .views.user.login import authenticateLoginInfo
 
@@ -16,6 +18,8 @@ urlpatterns = [
     path('excursionListPage/', getExcursionList, name='excursionListPage'),
     path('ExcursionPage/<int:pk>/', openExcursion, name='ExcursionPage'),
     path('PlaylistPage/<int:pk>/', openExcursionPlaylist, name='PlaylistPage'),
+    path('PlaylistItemAddPage/<int:pk>/', openPlaylistItemAddPage, name='PlaylistItemAddPage'),
+    path('PlaylistItem/<int:pk>/<int:item_id>/delete/', deletePlaylistItem, name='deletePlaylistItem'),
     path('PupilsListPage/<int:pk>/', pupilsListPage, name='PupilsListPage'),
     path('CreateExcursionPage/', addExcursion, name='CreateExcursionPage'),
     path('JoinExcursionPage/', openJoinExcursionPage, name='JoinExcursionPage'),
