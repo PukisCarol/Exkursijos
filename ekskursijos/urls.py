726bf3428eafaccb7d45de8b0b396c4d33b63bf4
gane347 <agnemilerytee@gmail.com>
2026-05-11 22:08:43 +0300

    Daiktu posisteme (#3)
    * added item subsystem functions
    
    * added docker files
    
    * reverted database file to avoid conflicts

--- ekskursijos/urls.py
+++ ekskursijos/urls.py
@@ -11,6 +11,18 @@ from .views.user.excursion import (
     openExcursionPlaylist,
     openPlaylistItemAddPage,
 )
+from .views.teacher.teacherItem import (
+    openTeacherItemList,
+    itemSelected,
+    editItem,
+    newItemSelected,
+    addItem,
+    deleteItem,
+    createNewLists,
+)
+from .views.pupil.pupilItem import (
+    openPupilItemList,
+)
 from .views.user.login import authenticateLoginInfo
 
 urlpatterns = [
@@ -21,6 +33,14 @@ urlpatterns = [
     path('PlaylistItemAddPage/<int:pk>/', openPlaylistItemAddPage, name='PlaylistItemAddPage'),
     path('PlaylistItem/<int:pk>/<int:item_id>/delete/', deletePlaylistItem, name='deletePlaylistItem'),
     path('PupilsListPage/<int:pk>/', pupilsListPage, name='PupilsListPage'),
+    path('TeacherItemPage/<int:pk>/', openTeacherItemList, name='TeacherItemPage'),
+    path('TeacherItemPage/<int:pk>/itemSelected/', itemSelected, name='itemSelected'),
+    path('TeacherItemPage/<int:pk>/editItem/', editItem, name='editItem'),
+    path('TeacherItemPage/<int:pk>/newItemSelected/', newItemSelected, name='newItemSelected'),
+    path('TeacherItemPage/<int:pk>/addItem/', addItem, name='addItem'),
+    path('TeacherItemPage/<int:pk>/deleteItem/', deleteItem, name='deleteItem'),
+    path('TeacherItemPage/<int:pk>/createNewLists/', createNewLists, name='createNewLists'),
+    path('PupilItemPage/<int:pk>/', openPupilItemList, name='openPupilItemList'),
     path('CreateExcursionPage/', addExcursion, name='CreateExcursionPage'),
     path('JoinExcursionPage/', openJoinExcursionPage, name='JoinExcursionPage'),
     path('<int:pk>/JoinExcursionPage/', authenticateLoginInfo, name='JoinExcursionPage'),
