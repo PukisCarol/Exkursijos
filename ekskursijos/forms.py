4e07cc3caee1c7f140e2c1d5f38860cd6cd44c4d
agne <agnemilerytee@gmail.com>
2026-05-11 21:37:32 +0300

    added item subsystem functions

--- ekskursijos/forms.py
+++ ekskursijos/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from .models.models import Excursion
-
+from .models.models import Excursion, SharedBackpackItem, Item
 
 class ExcursionForm(forms.ModelForm):
     class Meta:
@@ -17,3 +16,26 @@ class PublishExcursionForm(forms.Form):
         widget=forms.DateInput(attrs={'type': 'date'}),
         label='Excursion date',
     )
+
+
+class SharedBackpackItemForm(forms.ModelForm):
+    class Meta:
+        model = SharedBackpackItem
+        fields = ['item', 'importance', 'weight']
+        widgets = {
+            'importance': forms.NumberInput(attrs={'min': '1', 'max': '10'}),
+            'weight': forms.NumberInput(attrs={'min': '0'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        excursion = kwargs.pop('excursion', None)
+        super().__init__(*args, **kwargs)
+        if excursion:
+            # Only show items not already in the excursion
+            existing_items = SharedBackpackItem.objects.filter(
+                excursion=excursion
+            ).values_list('item_id', flat=True)
+            queryset = Item.objects.exclude(id__in=existing_items)
+            if self.instance and self.instance.pk:
+                queryset = queryset | Item.objects.filter(pk=self.instance.item_id)
+            self.fields['item'].queryset = queryset.distinct()
--- ekskursijos/urls.py
+++ ekskursijos/urls.py
@@ -8,6 +8,18 @@ from .views.user.excursion import (
     openJoinExcursionPage,
     pupilsListPage
 )
+from .views.teacher.teacherItem import (
+    openTeacherItemList,
+    itemSelected,
+    editItem,
+    newItemSelected,
+    addItem,
+    deleteItem,
+    createNewLists,
+)
+from .views.pupil.pupilItem import (
+    openPupilItemList,
+)
 from .views.user.login import authenticateLoginInfo
 
 urlpatterns = [
@@ -15,6 +27,14 @@ urlpatterns = [
     path('excursionListPage/', getExcursionList, name='excursionListPage'),
     path('ExcursionPage/<int:pk>/', openExcursion, name='ExcursionPage'),
     path('PupilsListPage/<int:pk>/', pupilsListPage, name='PupilsListPage'),
+    path('TeacherItemPage/<int:pk>/', openTeacherItemList, name='TeacherItemPage'),
+    path('TeacherItemPage/<int:pk>/itemSelected/', itemSelected, name='itemSelected'),
+    path('TeacherItemPage/<int:pk>/editItem/', editItem, name='editItem'),
+    path('TeacherItemPage/<int:pk>/newItemSelected/', newItemSelected, name='newItemSelected'),
+    path('TeacherItemPage/<int:pk>/addItem/', addItem, name='addItem'),
+    path('TeacherItemPage/<int:pk>/deleteItem/', deleteItem, name='deleteItem'),
+    path('TeacherItemPage/<int:pk>/createNewLists/', createNewLists, name='createNewLists'),
+    path('PupilItemPage/<int:pk>/', openPupilItemList, name='openPupilItemList'),
     path('CreateExcursionPage/', addExcursion, name='CreateExcursionPage'),
     path('JoinExcursionPage/', openJoinExcursionPage, name='JoinExcursionPage'),
     path('<int:pk>/JoinExcursionPage/', authenticateLoginInfo, name='JoinExcursionPage'),
